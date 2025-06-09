05079e7338f5be6e919b22c9852cd9b7b3f7f426
amin <amin1382422@gmail.com>
2025-06-10 00:44:18 +0330

    Initial commit

---
+++ lib/expanses.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Expanses extends StatefulWidget {
+  const Expanses({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ExpansesState();
+}
+
+class _ExpansesState extends State<Expanses> {
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Text('data'));
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:expense_tracker/expanses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Expanses()));
+}
